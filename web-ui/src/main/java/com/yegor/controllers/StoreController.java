a37e9493c8839cb4991c95ff0354dc036d9cc3cb
YegorKost <egor.kost@gmail.com>
2017-04-11 12:46:51 +0300

    dell redundant import

--- web-ui/src/main/java/com/yegor/controllers/StoreController.java
+++ web-ui/src/main/java/com/yegor/controllers/StoreController.java
@@ -2,20 +2,14 @@ package com.yegor.controllers;
 
 import com.yegor.entity.LaptopEntity;
 import com.yegor.service.LaptopService;
-import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.List;
